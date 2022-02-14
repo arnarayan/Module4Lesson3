bcac4cd210675805c1ec8eb990f17c566fefb5e7
Anand.Narayan <arnarayan@gmail.com>
2022-02-13 18:50:56 -0700

    Create challenge for Module4 Lesson 3

--- Module4Lesson3/ContentView.swift
+++ Module4Lesson3/ContentView.swift
@@ -13,19 +13,31 @@ struct ContentView: View {
     @State var tabIndex: Int = 2
     var body: some View {
         TabView(selection: $tabIndex){
-            Text("Hello world").tabItem {
+            Text("This tab's tag is \(tabIndex)").tabItem {
                 VStack {
-                    Image(systemName: "pencil")
-                    Text("Edit")
+                    Image(systemName: "tortoise")
+                    Text("Tab 1")
                 }
             }.tag(0)
-            Text("Hello world2").tabItem {
+            VStack{
+                Text("This tab's tag is \(tabIndex)")
+                Button(action: {
+                    tabIndex = 2
+                }, label: {
+                    Text("Take me to tab 3")
+                })
+            }
+            .tabItem {
                 VStack {
-                    Image(systemName: "heart")
-                    Text("save")
+                    Image(systemName: "arrow.right")
+                    Text("Tab 2")
                 }
             }.tag(1)
-            Text("This is delete operation").tabItem {
+            VStack {
+                Text("This tab's tag is \(tabIndex)")
+                Text("This is delete operation")
+            }
+            .tabItem {
                 VStack {
                     Image(systemName: "trash.circle.fill")
                     Text("delete")
